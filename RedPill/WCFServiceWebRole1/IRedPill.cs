d5669a26951614428b4cc3e9b0c44c7614d0a052
qianhaopower <qianhaopower@gmail.com>
2015-02-07 12:48:26 +1100

    initial check in

---
+++ RedPill/WCFServiceWebRole1/IRedPill.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFServiceWebRole1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+     [System.ServiceModel.ServiceContractAttribute(Namespace = "http://KnockKnock.readify.net", ConfigurationName = "RedifyService.IRedPill")]
+    public interface IRedPill
+    {
+
+        
+       
+        [System.ServiceModel.OperationContractAttribute(Action = "http://KnockKnock.readify.net/IRedPill/WhatIsYourToken", ReplyAction = "http://KnockKnock.readify.net/IRedPill/WhatIsYourTokenResponse")]
+        System.Guid WhatIsYourToken();
+
+
+      
+        [System.ServiceModel.OperationContractAttribute(Action = "http://KnockKnock.readify.net/IRedPill/FibonacciNumber", ReplyAction = "http://KnockKnock.readify.net/IRedPill/FibonacciNumberResponse")]
+        [System.ServiceModel.FaultContractAttribute(typeof(System.ArgumentOutOfRangeException), Action = "http://KnockKnock.readify.net/IRedPill/FibonacciNumberArgumentOutOfRangeException" +
+            "Fault", Name = "ArgumentOutOfRangeException", Namespace = "http://schemas.datacontract.org/2004/07/System")]
+        long FibonacciNumber(long n);
+
+        [System.ServiceModel.OperationContractAttribute(Action = "http://KnockKnock.readify.net/IRedPill/WhatShapeIsThis", ReplyAction = "http://KnockKnock.readify.net/IRedPill/WhatShapeIsThisResponse")]
+        TriangleType WhatShapeIsThis(int a, int b, int c);
+
+
+        [System.ServiceModel.OperationContractAttribute(Action = "http://KnockKnock.readify.net/IRedPill/ReverseWords", ReplyAction = "http://KnockKnock.readify.net/IRedPill/ReverseWordsResponse")]
+        [System.ServiceModel.FaultContractAttribute(typeof(System.ArgumentNullException), Action = "http://KnockKnock.readify.net/IRedPill/ReverseWordsArgumentNullExceptionFault", Name = "ArgumentNullException", Namespace = "http://schemas.datacontract.org/2004/07/System")]
+        
+        string ReverseWords(string s);
+
+
+    
+
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+
+    [System.Runtime.Serialization.DataContractAttribute(Name = "TriangleType", Namespace = "http://KnockKnock.readify.net")]
+    public enum TriangleType
+    {
+        [System.Runtime.Serialization.EnumMemberAttribute()]
+        Error = 0,
+
+        [System.Runtime.Serialization.EnumMemberAttribute()]
+        Equilateral = 1,
+
+        [System.Runtime.Serialization.EnumMemberAttribute()]
+        Isosceles = 2,
+
+        [System.Runtime.Serialization.EnumMemberAttribute()]
+        Scalene = 3,
+    }
+
+   
+}
---
+++ RedPill/WCFServiceWebRole1/RedPill.svc.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace WCFServiceWebRole1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class RedPill : IRedPill
+    {
+
+
+
+        public Guid WhatIsYourToken()
+        {
+            return Guid.Parse("b76d1eb4-9575-4b69-9578-b6d770ac439c");
+        }
+
+
+        [FaultContractAttribute(typeof(System.ArgumentOutOfRangeException))]
+        public long FibonacciNumber(long n)
+        {
+            if (n > 92) throw new ArgumentOutOfRangeException("n", "Fib(>92) will cause a 64-bit integer overflow.");
+
+            return FasterFibonacci(n);
+        }
+
+
+        /**
+             * basd on the fact
+             * F(2k) = F(k)[2F(k+1) – F(k)]
+             * F(2k+1) = F(k+1)2 + F(k)2
+             * proof can be found at http://www.m-hikari.com/imf-password2008/1-4-2008/ulutasIMF1-4-2008.pdf
+             * **/
+        private long FasterFibonacci(long n)
+        {
+            if (n == 0) return 0;
+            if (n <= 2)
+                return 1;
+            long k = n / 2;
+            long a = FasterFibonacci(k + 1);
+            long b = FasterFibonacci(k);
+
+            if (n % 2 == 1)
+                return a * a + b * b;
+            else
+                return b * (2 * a - b);
+        }
+
+
+
+        public TriangleType WhatShapeIsThis(int a, int b, int c)
+        {
+            if (a + b > c && b + c > a && a + c > b)
+            {
+                if (a == b && b == c)
+                {
+                    return TriangleType.Equilateral;
+                }
+                else if (a == b || b == c || c == a)
+                {
+                    return TriangleType.Isosceles;
+                }else
+                {
+                    return TriangleType.Scalene;
+                }
+
+            }
+            else
+            {
+                return TriangleType.Error;
+            }
+        }
+
+
+        [System.ServiceModel.FaultContractAttribute(typeof(System.ArgumentNullException))]
+        public string ReverseWords(string s)
+        {
+            if (s == null) throw new ArgumentNullException("s", "Value cannot be null.");
+
+            var textOriginal = s;
+            string temp = textOriginal;
+
+            int i = 0;
+            while (temp.EndsWith("\r\n"))
+            {
+                temp = temp.Remove(temp.Length - 2);
+                i++;
+            }
+            //i is how many \r\n in the last part of string.
+
+            string[] lines = Regex.Split(textOriginal, "\r\n|\r|\n");
+
+            StringBuilder builder = new StringBuilder();
+            foreach (var line in lines)
+            {
+                var reversedWords = string.Join(" ", line.Split(' ').Select(x => new String(x.Reverse().ToArray())));
+                builder.Append(string.Concat(reversedWords));
+                builder.Append("\r\n");
+            }
+            var bigResult = builder.ToString();
+
+            bigResult = bigResult.TrimEnd("\r\n".ToArray());
+            for (int j = 0; j < i; j++)
+            {
+                bigResult = bigResult + "\r\n";
+            }
+            return bigResult;
+        }
+
+
+        
+    }
+}
